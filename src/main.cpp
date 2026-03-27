9b9cd6558c9498882bc48a6b236c5991dae112fa
Jules-G-code <jules.gautier@gmail.com>
2026-03-27 18:50:43 +0100

    Capteur qui ne fonctionne pas

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,38 @@
+
 #include <Arduino.h>
+#include "rgb_lcd.h"
+#include <ESP32Encoder.h>
+#include <Wire.h>
+#include <Adafruit_TCS34725.h>
+#include <ESP32Servo.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 
-// put function declarations here:
-int myFunction(int, int);
+Adafruit_TCS34725 tcs;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+    Serial.begin(115200);
+    Wire.begin(21, 22); // SDA sur 21, SCL sur 22
+
+    if (tcs.begin()) {
+        Serial.println("Capteur TCS34725 trouvé");
+    } else {
+        Serial.println("Aucun TCS34725 trouvé ... vérifiez vos connexions");
+        while (1); // Arrêt
+    }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+    uint16_t r, g, b, c;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    tcs.getRawData(&r, &g, &b, &c);
+
+    Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
+    Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
+    Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
+    Serial.print("C: "); Serial.print(c, DEC); Serial.println(" ");
+
+    delay(1000);
 }
