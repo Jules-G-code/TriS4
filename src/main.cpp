2a6addb17f1f65640b8805ff4efa51df10affda3
Jules-G-code <jules.gautier@gmail.com>
2026-05-04 11:45:08 +0200

    capteur OK

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,60 @@
 #include <Adafruit_Sensor.h>
 
 Adafruit_TCS34725 tcs;
+rgb_lcd lcd;
 
+
+void setup()
+{
+    Serial.begin(115200);
+    Wire.begin(21, 22); // SDA sur 21, SCL sur 22
+
+    pinMode(25, OUTPUT); // Bleu
+    pinMode(26, OUTPUT); // Rouge
+    pinMode(27, OUTPUT); // Vert
+
+}
+
+void loop()
+{
+    uint16_t r, g, b, c, color;
+    tcs.getRawData(&r, &g, &b, &c);
+    
+    color = tcs.calculateColorTemperature_dn40(r, g, b, c);
+    Serial.printf("colorTemp = %d\n", color);
+
+    // Activation des sorties en fonction des seuils
+    if (color < 100) {
+        digitalWrite(25, HIGH); // Bleu
+        digitalWrite(26, LOW);
+        digitalWrite(27, LOW);
+    } else if (color >= 1000 && color <= 2000) {
+        digitalWrite(25, LOW);
+        digitalWrite(26, HIGH); // Rouge
+        digitalWrite(27, LOW);
+    } else if (color > 5000) {
+        digitalWrite(25, LOW);
+        digitalWrite(26, LOW);
+        digitalWrite(27, HIGH); // Vert
+    } else {
+        digitalWrite(25, LOW);
+        digitalWrite(26, LOW);
+        digitalWrite(27, LOW);
+    }
+
+    // Lire l'état des sorties
+    int state25 = digitalRead(25);
+    int state26 = digitalRead(26);
+    int state27 = digitalRead(27);
+    Serial.printf("Pin 25 (Bleu): %d, Pin 26 (Rouge): %d, Pin 27 (Vert): %d\n", state25, state26, state27);
+   
+    delay(400);
+}
+
+
+
+
+/*
 void setup()
 {
     Serial.begin(115200);
@@ -36,3 +89,4 @@ void loop()
 
     delay(1000);
 }
+*/
