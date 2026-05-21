c0118f08ee6657afb2a2c2063a02a94148b60e0b
Jules-G-code <jules.gautier@gmail.com>
2026-05-21 17:30:59 +0200

    Code Final

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,14 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 
-Adafruit_TCS34725 tcs;
+Adafruit_TCS34725 tcs= Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_101MS, TCS34725_GAIN_4X);
 rgb_lcd lcd;
 
 
 void setup()
 {
     Serial.begin(115200);
-    Wire.begin(21, 22); // SDA sur 21, SCL sur 22
+   
 
     pinMode(25, OUTPUT); // Bleu
     pinMode(26, OUTPUT); // Rouge
@@ -45,32 +45,32 @@ void loop()
     uint16_t r, g, b, c, color;
     tcs.getRawData(&r, &g, &b, &c);
     
-    color = tcs.calculateColorTemperature_dn40(r, g, b, c);
-    Serial.printf("colorTemp = %d\n", color);
+    //tcs.calculateColorTemperature_dn40(r, g, b, c);
+    Serial.printf("r=%d,g=%d,b=%d,\n", r,g,b);
 
     // Activation des sorties en fonction des seuils
-    if (color < 100) {
-        digitalWrite(25, LOW); // Bleu
-        digitalWrite(26, HIGH);
-        digitalWrite(27, HIGH);
-    } else if (color >= 1000 && color <= 2000) {
-        digitalWrite(25, HIGH);
-        digitalWrite(26, LOW); // Rouge
-        digitalWrite(27, HIGH);
-    } else if (color > 5000) {
-        digitalWrite(25, HIGH);
-        digitalWrite(26, HIGH);
-        digitalWrite(27, LOW); // Vert
+    if (r<90 && g<90 && b>100 ) {
+        digitalWrite(25, HIGH); // Bleu
+        digitalWrite(26, LOW);
+        digitalWrite(27, LOW);
+    } else if (r>190 && g<120 && b<120) {
+        digitalWrite(25, LOW);
+        digitalWrite(26, HIGH); // Rouge
+        digitalWrite(27, LOW);
+    } else if (r<180 && g>210 && b<180) {
+        digitalWrite(25, LOW);
+        digitalWrite(26, LOW);
+        digitalWrite(27, HIGH); // Vert
     } else {
-        digitalWrite(25, HIGH);
-        digitalWrite(26, HIGH);
-        digitalWrite(27, HIGH);
+        digitalWrite(25, LOW);
+        digitalWrite(26, LOW);
+        digitalWrite(27, LOW);
     }
 
     // Lire l'état des sorties
     int state25 = digitalRead(25);
     int state26 = digitalRead(26);
-    int state27 = digitalRead(27);
+    int state27 = digitalRead(27); 
     Serial.printf("Pin 25 (Bleu): %d, Pin 26 (Rouge): %d, Pin 27 (Vert): %d\n", state25, state26, state27);
    
     delay(400);
